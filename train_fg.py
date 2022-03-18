bae24d5a83b4d710eac93f03ad39174901e06b90
IJCAI22-5256 <75292765+ijcai-ai@users.noreply.github.com>
2022-03-18 21:14:15 +0800

    Update train_fg.py

--- train_fg.py
+++ train_fg.py
@@ -8,15 +8,16 @@ from torch.autograd import Variable
 import torch.nn as nn
 from pathlib import Path
 import warnings
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "3"
-
-
+# os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+#
 warnings.filterwarnings('ignore')
 mainpath = os.getcwd()
 pix2pixhd_dir = Path(mainpath + '/src/pix2pixHD/')
 sys.path.append(str(pix2pixhd_dir))
 
-from data.data_loader import CreateDataLoader
+from enhancer.src_enhance.pix2pixHD.data.data_loader import CreateDataLoader
 from models.models import create_model
 import util.util as util
 from util.visualizer import Visualizer
@@ -28,6 +29,11 @@ torch.backends.cudnn.benchmark = True
 
 opt.checkpoints_dir += 'fg/'
 opt.target = 'fg'
+
+
+def freeze_plane2(model):
+    for param in model.parameters():
+        param.requires_grad = False
 
 
 def main():
@@ -43,43 +49,39 @@ def main():
     save_delta = total_steps % opt.save_latest_freq
 
     model_fg = create_model(opt)
+    # model_fg = nn.DataParallel(model_fg)
     model_fg = model_fg.cuda()
     visualizer = Visualizer(opt)
 
     ###
     checkpoint_folder = os.path.join(opt.checkpoints_dir, opt.name)
 
-    files = [file_name for file_name  in next(os.walk(checkpoint_folder))[2]]
+    files = [file_name for file_name in next(os.walk(checkpoint_folder))[2]]
 
     files = [file_name for file_name in files if file_name.endswith("pth")]
 
     max_epoch = 0
     for file_name in files:
         temp_epoch = file_name.split("_")[0]
-        if temp_epoch !="latest":
+        if temp_epoch != "latest":
             epoch_num = int(temp_epoch)
-            max_epoch =epoch_num if epoch_num>max_epoch else max_epoch
+            max_epoch = epoch_num if epoch_num > max_epoch else max_epoch
 
-    if max_epoch!=0:
+    if max_epoch != 0:
         start_epoch = max_epoch
-        Dfg_checkpoint_path = os.path.join(checkpoint_folder,f"{max_epoch}_net_Dfg.pth")
-        Dtfg_checkpoint_path = os.path.join(checkpoint_folder,f"{max_epoch}_net_Dtfg.pth")
-        Gfg_checkpoint_path = os.path.join(checkpoint_folder,f"{max_epoch}_net_Gfg.pth")
+        Dfg_checkpoint_path = os.path.join(checkpoint_folder, f"{max_epoch}_net_Dfg.pth")
+        Dtfg_checkpoint_path = os.path.join(checkpoint_folder, f"{max_epoch}_net_Dtfg.pth")
+        Gfg_checkpoint_path = os.path.join(checkpoint_folder, f"{max_epoch}_net_Gfg.pth")
 
-        D_optimizer_path =os.path.join(checkpoint_folder,f"{max_epoch}_optimizer_D.pth")
-        Dt_optimizer_path =os.path.join(checkpoint_folder,f"{max_epoch}_optimizer_Dt.pth")
-        G_optimizer_path =os.path.join(checkpoint_folder,f"{max_epoch}_optimizer_G.pth")
+        D_optimizer_path = os.path.join(checkpoint_folder, f"{max_epoch}_optimizer_D.pth")
+        Dt_optimizer_path = os.path.join(checkpoint_folder, f"{max_epoch}_optimizer_Dt.pth")
+        G_optimizer_path = os.path.join(checkpoint_folder, f"{max_epoch}_optimizer_G.pth")
         model_fg.netD.load_state_dict(torch.load(Dfg_checkpoint_path))
         model_fg.netDt.load_state_dict(torch.load(Dtfg_checkpoint_path))
         model_fg.netG.load_state_dict(torch.load(Gfg_checkpoint_path))
         model_fg.optimizer_D.load_state_dict(torch.load(D_optimizer_path))
         model_fg.optimizer_Dt.load_state_dict(torch.load(Dt_optimizer_path))
         model_fg.optimizer_G.load_state_dict(torch.load(G_optimizer_path))
-
-
-
-
-
 
     ###
 
@@ -88,7 +90,7 @@ def main():
         if epoch != start_epoch:
             epoch_iter = epoch_iter % dataset_size
         for i, data in enumerate(dataset, start=epoch_iter):
-            torch.cuda.empty_cache()
+            # torch.cuda.empty_cache()
             iter_start_time = time.time()
             total_steps += opt.batchSize
             epoch_iter += opt.batchSize
@@ -98,13 +100,37 @@ def main():
 
             losses, generated = model_fg(Variable(data['label']), Variable(data['inst']), Variable(data['mask']),
                                          Variable(data['image']), Variable(data['feat']), infer=True)
+            ###
+            import torchvision
+            import optimal_transport_loss as ot
+            opt_loss = ot.FeatureOptimalLoss()
+            res_net = torchvision.models.resnet18(pretrained=True)
+            res_net.cuda()
+            freeze_plane2(res_net)
+            # new_gen = torch.nn.functional.interpolate(generated, size = (328, 328), mode='bilinear')
+            # new_gen = torch.nn.functional.interpolate(generated, scale_factor = 2, mode='bilinear')
+            # new_gt = torch.nn.functional.interpolate(data['image'], scale_factor = 2, mode='nearest')
+            # new_gt = torch.nn.functional.interpolate(data['gt_img'], size = (328, 328), mode='bilinear')
+            new_gen = generated.cuda()
+            new_gt = data['gt_img'].cuda()
+            gen_fearture = res_net(new_gen)
+            gt_fearture = res_net(new_gt)
 
+            ####
+
+
+
+
+            loss_feature_alignment = opt_loss(gen_fearture, gt_fearture)
+            print("epoch:%d,i:%d,loss_feature_alignment:%f"%(epoch,i,loss_feature_alignment / 100) )
+            ###
             losses = [torch.mean(x) if not isinstance(x, int) else x for x in losses]
             loss_dict = dict(zip(model_fg.loss_names, losses))
 
             loss_D_fg = (loss_dict['D_fake'] + loss_dict['D_real']) * 0.5
             loss_Dt_fg = (loss_dict['Dt_fake'] + loss_dict['Dt_real']) * 0.5
-            loss_G_fg = loss_dict['G_GAN'] + loss_dict.get('G_GAN_Feat', 0) + loss_dict.get('G_VGG', 0) + loss_dict['Dt_fake'] * 0.25
+            loss_G_fg = loss_dict['G_GAN'] + loss_dict.get('G_GAN_Feat', 0) + loss_dict[
+                'Dt_fake'] * 0.25 + loss_feature_alignment * 0.1
 
             model_fg.optimizer_G.zero_grad()
             loss_G_fg.backward(retain_graph=True)
@@ -137,8 +163,6 @@ def main():
             if total_steps % opt.save_latest_freq == save_delta:
                 print('saving the latest model (epoch %d, total_steps %d)' % (epoch, total_steps))
 
-
-
                 model_fg.save('latest')
                 np.savetxt(iter_path, (epoch, epoch_iter), delimiter=',', fmt='%d')
 
@@ -159,14 +183,9 @@ def main():
             optimizer_Dt = model_fg.optimizer_Dt
             optimizer_G = model_fg.optimizer_G
 
-            # torch.save(optimizer_D.state_dict(),f"{epoch}_optimizer_D.pth")
-            # torch.save(optimizer_Dt.state_dict(),f"{epoch}_optimizer_Dt.pth")
-            # torch.save(optimizer_G.state_dict(),f"{epoch}_optimizer_G.pth")
-
-            torch.save(optimizer_D.state_dict(), os.path.join(checkpoint_folder, f"{epoch}_optimizer_D.pth"))
-            # torch.save(generator.state_dict(), os.path.join(self.ckpt_dir, '%05d_generator.pth' % batch_num))
-            torch.save(optimizer_Dt.state_dict(), os.path.join(checkpoint_folder, f"{epoch}_optimizer_Dt.pth"))
-            torch.save(optimizer_G.state_dict(), os.path.join(checkpoint_folder, f"{epoch}_optimizer_G.pth"))
+            torch.save(optimizer_D.state_dict(), f"{epoch}_optimizer_D.pth")
+            torch.save(optimizer_Dt.state_dict(), f"{epoch}_optimizer_Dt.pth")
+            torch.save(optimizer_G.state_dict(), f"{epoch}_optimizer_G.pth")
 
 
             np.savetxt(iter_path, (epoch + 1, 0), delimiter=',', fmt='%d')
